6061abddfc315423324692315807637b20ce5bc4
SW-H <hsw08@naver.com>
2022-08-21 20:38:16 +0900

    clean

--- APIs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import getVideoAPI
-
-# localhost/api/hello
-urlpatterns = [
-    path("hello/", getVideoAPI),
-    
-]
--- DataAnalysis/morphological_analysis.py
+++
@@ -1,6 +0,0 @@
-from konlpy.tag import Komoran
-komoran = Komoran()
-
-text = "나는 한국을 사랑한다. 나는 오늘 너무 행복하다."
-lst = komoran.pos(text)
-print(lst)
