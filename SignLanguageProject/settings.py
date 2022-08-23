59d6224eab25d6394cfab13fae213784a09a1776
SW-H <hsw08@naver.com>
2022-08-23 15:30:37 +0900

    refactor : CORS 정책 관련 설정

--- SignLanguageProject/settings.py
+++ SignLanguageProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'APIs',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+
 ]
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:8080",
+)
 
 ROOT_URLCONF = 'SignLanguageProject.urls'
 
